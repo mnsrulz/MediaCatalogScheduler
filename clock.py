8168c0f29210c303fa8927b9a3589f0d8a3f33c4
Manish Rawat <manish.rawat@nagarro.com>
2019-02-01 23:27:36 -0500

    log to db test

--- clock.py
+++ clock.py
@@ -1,10 +1,27 @@
+import sys
+import os
+import pymongo
 from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
 
 sched = BlockingScheduler()
+
+# Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+
+uri = os.environ['DB_CONN_LOG']
 
 @sched.scheduled_job('interval', minutes=1)
 def timed_job():
     print('This job is run every one minutes.')
+    client = pymongo.MongoClient(uri)
+    db = client.get_database()
+    logs = db['logs']
+    logs.insert({
+        'ts': datetime.utcnow(),
+        'desc': 'Running media scheduler cron'
+    })
+    client.close()
+
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
