43bb74d3b2c68bef2ef54a2a4997b53a49356202
manishrawat4u <manishrawat4u@gmail.com>
2019-02-08 02:26:58 -0500

    Update clock.py

--- clock.py
+++ clock.py
@@ -10,6 +10,9 @@ def google_drive_import_media_job():
     print('Executing google drive media import job.')
     dw.execute()
 
+    
+google_drive_import_media_job()
+
 
 # @blockingScheduler.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 # def scheduled_job():
